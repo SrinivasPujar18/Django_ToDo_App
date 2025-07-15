e560f3dd3731f1a31dadddc1fb9726632723767e
Srinivas Pujar <srinivaspujar2001@gmail.com>
2025-07-15 13:45:22 +0530

    message

--- Theory_django_working.py
+++ Theory_django_working.py
@@ -48,6 +48,8 @@
 # git branch -M main
 # git remote add origin https://github.com/SrinivasPujar18/Django_toDo_.git
 # git push -u origin main
+
+
 # …or push an existing repository from the command line
 # git remote add origin https://github.com/SrinivasPujar18/Django_toDo_.git
 # git branch -M main
